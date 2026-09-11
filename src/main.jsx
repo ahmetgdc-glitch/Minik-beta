a0b69e4022be1f8c936108c37bd156195419ca95
ahmetgdc-glitch <ahmetgdc@gmail.com>
2026-09-11 10:00:54 +0200

    Load immersive opposites playground styles

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ import "./games/routine-journey.css";
 import "./games/memory-playground.css";
 import "./games/difference-playground.css";
 import "./games/missing-stage.css";
+import "./games/opposites-playground.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
