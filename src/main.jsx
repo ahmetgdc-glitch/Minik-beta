d2d517bfbb6ad5a1112fe8a711d5a4d6ead5a4e6
ahmetgdc-glitch <ahmetgdc@gmail.com>
2026-09-11 10:07:59 +0200

    Load immersive shadow cave styles

--- src/main.jsx
+++ src/main.jsx
@@ -28,6 +28,7 @@ import "./games/opposites-playground.css";
 import "./games/pattern-path.css";
 import "./games/letter-playground.css";
 import "./games/listen-playground.css";
+import "./games/shadow-playground.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
