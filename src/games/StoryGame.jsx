5a02e38cb4dd7e6f996d3dc8d1db7665c18befd6
ahmetgdc-glitch <ahmetgdc@gmail.com>
2026-09-17 09:32:21 +0200

    Keep story recall visual-first

--- src/games/StoryGame.jsx
+++ src/games/StoryGame.jsx
@@ -160,6 +160,7 @@ export default function StoryGame({
           </div>
           <OptionGrid
             {...{ options, target, hint, lang, settings }}
+            hiddenLabels={hint < 3}
             disabled={controlsDisabled}
             onPick={pick}
           />
